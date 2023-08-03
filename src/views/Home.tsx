21b96d09678bb089ce69acd8acb4e866e0a0326a
salvadoruriel <salvador.urielgm+git@gmail.com>
2023-08-02 22:44:55 -0600

    feat: adjust basic About section

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -10,11 +10,19 @@ export default function Welcome() {
 		<Stack component="main" sx={{ alignItems: 'center' }}>
 			{isLoading && <Loader />}
 			{/* About the page */}
-			<Stack component="section" aria-label="About this page" sx={{ gap: '10px', pt: '30px' }}>
-				<Typography variant="h4" component={'h1'} sx={{ pt: '100px', textAlign: 'center' }}>
+			<Stack
+				component="section"
+				aria-label="About this page"
+				sx={{ gap: '40px', p: '20px', pt: '130px', alignItems: 'center' }}
+			>
+				<Typography variant="h2" component={'h1'} sx={{ textAlign: 'center' }}>
 					{'Rick & Morty searcher'}
 				</Typography>
+				<Typography sx={{ maxWidth: '400px', textAlign: 'center' }}>
+					{`Hello! This page is a simple searcher for characters, locations & episodes from the series "Rick and morty"`}
+				</Typography>
 			</Stack>
+
 		</Stack>
 	);
 }
