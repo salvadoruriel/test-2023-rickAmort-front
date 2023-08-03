fd927aab204adfd7726a198749d3ba2cd903fc68
salvadoruriel <salvador.urielgm+git@gmail.com>
2023-08-03 02:54:46 -0600

    style: make CharAvatar more interactible when hovered

--- src/components/CharAvatar.tsx
+++ src/components/CharAvatar.tsx
@@ -7,11 +7,24 @@ export type t_CharAvatar = AvatarProps & {
 };
 /**Styled character image displayer & Modal displayer */
 export default function CharAvatar(props: t_CharAvatar) {
-	const { char, ...avatarProps } = props;
+	const { char, sx, ...avatarProps } = props;
 
 	if (!char) return null;
 	return (
-		<Avatar variant="rounded" alt={char.name} src={char.image} {...avatarProps}>
+		<Avatar
+			variant="rounded"
+			alt={char.name}
+			src={char.image}
+			sx={{
+				transition: 'transform 0.2s ease-in-out',
+				'&:hover': {
+					transform: 'scale(1.2)',
+				},
+				cursor: 'pointer',
+				...sx,
+			}}
+			{...avatarProps}
+		>
 			{char.name}
 		</Avatar>
 	);
